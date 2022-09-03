6ad652cf9442e7fa39c0cbb6219c074460d5ef70
DomDom <dominicfisher182001@gmail.com>
2022-09-02 21:05:04 -0400

    added project 3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import { useState } from 'react';
 import Nav from './components/Nav';
+import Body from './components/Nav/Body';
 
 function App() {
-  const [page, setPage] = useState('about');
+  // const [page, setPage] = useState('about');
 
 
   return (
     <>
       <Nav />
+      <Body />
     </>
   );
 }
---
+++ src/components/Nav/Body.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Body = props => (
+    <div>
+        <a href="https://level-up-coding.herokuapp.com/">Level-Up-Coding</a>
+        <p>
+            Stay tuned, will be finishing this portfolio soon!
+        </p>
+    </div>
+);
+
+export default Body;
